7012cf6b42d06a0781d9604207d5b09c26098c4b
stephband <stephband@googlemail.com>
2015-08-11 23:13:38 +0200

    Adds sequence.js

---
+++ src/sequence.js
@@ -0,0 +1,19 @@
+(function(window) {
+	"use strict";
+
+	var assign = Object.assign;
+
+	function Sequence(clock, data) {
+		
+	}
+
+	assign(Sequence.prototype, Clock.prototype, {
+		start: function(time) {
+			
+		},
+
+		stop: function(time) {
+			
+		}
+	});
+})(window);
